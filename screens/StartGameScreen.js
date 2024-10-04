894132db55d6e0df260ad035a71dd69bafd71ae2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-10-04 16:47:50 +0200

    feat: add attributes to TextInput in StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />
+            <TextInput 
+                style={styles.numberInput} 
+                autoCorrect={false} 
+                autoCapitalize="none" 
+                keyboardType="number-pad" 
+                maxLength={2}
+            />
             <PrimaryButton>Reset</PrimaryButton> 
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -16,12 +22,23 @@ const styles = StyleSheet.create({
         padding: 16,
         marginTop: 100,
         marginHorizontal: 24,
-        backgroundColor: '#72063c',
+        backgroundColor: '#4e0329',
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center'
     }
 });
